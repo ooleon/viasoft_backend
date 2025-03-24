6b2bc9eedcfcaa3f5539725c62500f091181ae2e
ooleon <ooleon@hotmail.com>
2025-03-24 17:09:47 -0300

    run base

---
+++ viasoftemail/src/main/java/localhost/ViasoftEmailApplication.java
@@ -0,0 +1,10 @@
+package localhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViasoftEmailApplication {
+
+	public static void main(String[] args) {SpringApplication.run(ViasoftEmailApplication.class, args);}
+}
